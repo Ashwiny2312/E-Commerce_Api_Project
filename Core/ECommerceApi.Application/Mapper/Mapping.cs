d0c90128f09a8926367487d707725558b483bd80
oğuzhan kömcü <komcuoguzz@gmail.com>
2022-07-17 18:22:04 +0300

    I created my repository concretes.

---
+++ Core/ECommerceApi.Application/Mapper/Mapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ECommerceApi.Application.CQRS.Product.Commands.Request;
+using ECommerceApi.Application.CQRS.Product.Queries.Response;
+using ECommerceApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.Mapper
+{
+    public class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<CreateProductCommandRequest, Product>();
+            CreateMap<Product, CreateProductCommandRequest>();
+            CreateMap<Product, GetByIdProductQueryResponse>();
+            CreateMap<Product, List<GetAllProductQueryResponse>>();
+        }
+    }
+}
