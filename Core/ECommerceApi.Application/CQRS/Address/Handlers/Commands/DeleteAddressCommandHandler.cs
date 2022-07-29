174daf269aba003031724dd439bbfbd50eb49131
oğuzhan kömcü <komcuoguzz@gmail.com>
2022-07-29 21:27:24 +0300

    I'm doing crud operations of my address entity.

---
+++ Core/ECommerceApi.Application/CQRS/Address/Handlers/Commands/DeleteAddressCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using ECommerceApi.Application.CQRS.Address.Commands.Request;
+using ECommerceApi.Application.CQRS.Address.Commands.Response;
+using ECommerceApi.Application.RepositoriesInterface;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.CQRS.Address.Handlers.Commands
+{
+
+
+    public class DeleteAddressCommandHandler : IRequestHandler<DeleteAddressCommandRequest, DeleteAddressCommandResponse>
+    {
+        private readonly IAddressRepository _addressRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteAddressCommandHandler(IAddressRepository addressRepository, IMapper mapper)
+        {
+            _addressRepository = addressRepository;
+            _mapper = mapper;
+        }
+
+
+        public async Task<DeleteAddressCommandResponse> Handle(DeleteAddressCommandRequest request, CancellationToken cancellationToken)
+        {
+
+
+            var category = await _addressRepository.GetDefault(x => x.Id == request.Id);
+
+            category.Status = Domain.Enums.Status.Passive;
+            category.DeleteDate = DateTime.Now;
+
+            await _addressRepository.Commit();
+
+            return new DeleteAddressCommandResponse
+            {
+                IsSuccess = true,
+            };
+        }
+    }
+}
