c53116166528d295081bb4a69b96ce6df05862b9
oğuzhan kömcü <komcuoguzz@gmail.com>
2022-08-01 21:42:41 +0300

    I'm doing Crud operations for my AppUser entity.

---
+++ Core/ECommerceApi.Application/CQRS/AppUser/Queries/Request/UserGetByIdQueryRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.CQRS.AppUser.Queries.Request
+{
+    public class UserGetByIdQueryRequest
+    {
+        public string UserId { get; set; }
+    }
+}
