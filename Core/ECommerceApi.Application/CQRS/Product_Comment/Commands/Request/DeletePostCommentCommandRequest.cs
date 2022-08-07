59637f69ff77a519b48ddafd8106f16d9952f6ab
oğuzhan kömcü <komcuoguzz@gmail.com>
2022-08-07 11:36:02 +0300

    I created a service for the "Delete" operation of Product Comment models.

---
+++ Core/ECommerceApi.Application/CQRS/Product_Comment/Commands/Request/DeletePostCommentCommandRequest.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceApi.Application.CQRS.Product_Comment.Commands.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.CQRS.Product_Comment.Commands.Request
+{
+    public class DeletePostCommentCommandRequest : IRequest<DeletePostCommentCommandResponse>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Core/ECommerceApi.Application/CQRS/Product_Comment/Handlers/Commands/DeletePostCommentCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using ECommerceApi.Application.CQRS.Product_Comment.Commands.Request;
+using ECommerceApi.Application.CQRS.Product_Comment.Commands.Response;
+using ECommerceApi.Application.RepositoriesInterface;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.CQRS.Product_Comment.Handlers.Commands
+{
+
+    public class DeletePostCommentCommandHandler : IRequestHandler<DeletePostCommentCommandRequest, DeletePostCommentCommandResponse>
+    {
+        private readonly IProductCommentRepository _productCommentRepository;
+        private readonly IMapper _mapper;
+
+        public DeletePostCommentCommandHandler(IProductCommentRepository productCommentRepository, IMapper mapper)
+        {
+            _productCommentRepository = productCommentRepository;
+            _mapper = mapper;
+        }
+
+
+        public async Task<DeletePostCommentCommandResponse> Handle(DeletePostCommentCommandRequest request, CancellationToken cancellationToken)
+        {
+
+
+            var category = await _productCommentRepository.GetDefault(x => x.Id == request.Id);
+
+            category.Status = Domain.Enums.Status.Passive;
+            category.DeleteDate = DateTime.Now;
+
+            await _productCommentRepository.Commit();
+
+            return new DeletePostCommentCommandResponse
+            {
+                IsSuccess = true,
+            };
+        }
+    }
+}
